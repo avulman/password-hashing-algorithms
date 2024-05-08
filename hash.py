18132cf41cf69dda8f9c622e435b5e0ed1027f41
avulman <vulmana@gmail.com>
2024-05-07 22:40:20 -0600

    .

--- hash.py
+++ hash.py
@@ -5,8 +5,10 @@ import secrets
 
 EXIT_STATEMENTS = ["Q", "QUIT", "EXIT"]
 HASHING_ALGORITHMS = ["MD5", "SHA-1", "SHA-2", "NTLM", "LANMAN"]
+first_program_iteration = True
 
 def print_intro():
+    global first_program_iteration
     print("╔═══════════════════════════════════════════════╗\n"
           "|   Welcome to the password hashing program!    |\n"
           "╞═══════════════════════════════════════════════╡\n"
@@ -14,6 +16,7 @@ def print_intro():
           "|   algorithms including: MD5, SHA-1, SHA-2,    |\n"
           "|   NTLM, and LANMAN.                           |\n"                        
           "╚═══════════════════════════════════════════════╝\n")
+    first_program_iteration = False
 
 def explain_algorithm():
     print("\nYou selected: » What is a hashing algorithm? «\n")
@@ -31,47 +34,58 @@ def learn_more(selected_algorithm):
 
 def hash_password(selected_algorithm):
     option_1_selected = False
+    option_2_selected = False
     salt_enabled = False
     print("\nYou selected » Hash a password using", selected_algorithm, "«\n")
     password_to_hash = ""
     while not password_to_hash:
-        password_to_hash = input("Select a password that is 1-16 characters in length.\n"
+        password_to_hash = input("Enter a password that is 1-16 characters in length.\n"
                                    "* Password you would like to hash: ")
         if not password_to_hash:
             print("Password must be at least 1 character.")
-        elif len(password_to_hash) > 16:
-            print("Password exceeds length criteria.")
+        elif len(password_to_hash) > 16 or len(password_to_hash) < 1:
+            print("Password must be 1-16 characters in length.")
         else:
             while not option_1_selected == True:
                 option = input("\n(1) Add a salt. \n"
                     "(2) Do not add a salt.\n"
                     "(3) What is a salt?\n"
                     "* Select an option: ")
+                pre_salt_password = password_to_hash
                 if option == "3":
                     print("A salt is a randomly generated string of characters that\n"
                         "is added to a password before it is hashed. The purpose of\n"
                         "adding a salt is to enhance the security of hashed passwords,\n"
                         "particularly against dictionary and rainbow table attacks.\n")
-                    option_1_selected = True
+                    option_1_selected = False
                 elif option == "1":
                     print("\nSalting your password!")
-                    pre_salt_password = password_to_hash
                     salt = generate_salt()
                     password_to_hash = salt_password(password_to_hash, salt)
                     salt_enabled = True
+                    option_1_selected = True
                 elif option == "2":
                     print("No worries.")
+                    option_2_selected = True
                 else:
                     invalid_selection()
 
-                print("Password:", pre_salt_password)
+                if  option_1_selected == True:
+                    print("Password:", pre_salt_password)
+                elif option_2_selected == True:
+                    print("Password:", password_to_hash)
+
                 if salt_enabled == True:
                     print("Salt:", salt)
                     salt_enabled = False
 
                 print("Hashing »", password_to_hash, "«\n")
-                hashed_password = md5(password_to_hash)
-                print(selected_algorithm, "Hashed Password: »", hashed_password, "«\n")
+                if selected_algorithm == "MD5":
+                    hashed_password = md5(password_to_hash)
+                else:
+                    print("This hash function has not been implemented yet!")
+
+                print("Your", selected_algorithm, "Hashed Password: »", hashed_password, "«\n")
                 break
 
 def generate_salt():
@@ -93,7 +107,7 @@ def selected_statement(option):
 
 def select_algorithm():
     while True:
-        option = input("* Enter an algorithm to learn more or 'Q' to quit:\n").upper()
+        option = input("* Enter an algorithm to learn more or 'Q' to quit: ").upper()
         if option in EXIT_STATEMENTS:
             print("Quitting... See you next time!")
             sys.exit(0)
@@ -144,7 +158,9 @@ def select_main_features(selected_algorithm):
 def main():
     # TODO implement to loop program in a clean manner.
     # completed_program = False 
-    print_intro()
+    if first_program_iteration == True:
+        print_intro()
+
     selected_algorithm = select_algorithm()
     select_main_features(selected_algorithm)
 
